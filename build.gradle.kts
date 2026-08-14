772c909840d9817718cc12edf05606ba03a659a7
xukur9033-del <xukur9033@gmail.com>
2026-08-15 07:37:43 +0800

    Add root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Top-level build file — 仅声明插件版本，不在此添加 dependencies。
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
